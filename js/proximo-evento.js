e72f6eac46dc1b5198638b230a116acb2ab770ea
Wilson Ramos <hupcontato@gmail.com>
2020-10-26 11:05:25 -0300

    Aagenda

--- js/proximo-evento.js
+++ js/proximo-evento.js
@@ -8,12 +8,15 @@ while (agenda[diaDoMes] == '') {
 for (var i = 0; i < agenda[diaDoMes].length; i++) {
     var post = `
     <div class="item-programacao">
-        <div class="bemvindo-dia">${agenda[diaDoMes][i].dia}</div>
-        <h6 class="bemvindo-titulo">${agenda[diaDoMes][i].evento}</h6>
-        <i class="fas fa-map-marker-alt"></i><span>${agenda[diaDoMes][i].local}</span><br>
-        <i class="far fa-calendar-alt"></i><span>${agenda[diaDoMes][i].dia == diaDoMesComp ? 'Hoje' : agenda[diaDoMes][i].semana}</span> - <span>${agenda[diaDoMes][i].hora}</span>
+        <span class="bemvindo-dia">${agenda[diaDoMes][i].dia}</span>
+        <div class="item-programacao-detalhes">
+            <h6 class="bemvindo-titulo">${agenda[diaDoMes][i].evento}</h6>
+            </i><span>${agenda[diaDoMes][i].local}</span><br>
+            <span>${agenda[diaDoMes][i].dia == diaDoMesComp ? 'Hoje' : agenda[diaDoMes][i].semana}</span> - <span>${agenda[diaDoMes][i].hora}</span>
+        </div>
     </div>
-    
     `
     $('.programacao').append(post)
 }
+var botaoVerTudo = `<a href="eventos.html">Ver Todos</a>`
+$('.programacao').append(botaoVerTudo)
