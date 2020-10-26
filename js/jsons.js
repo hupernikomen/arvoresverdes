e9d104d2912030b30d6274dd4303a1cd721cce47
Wilson Ramos <hupcontato@gmail.com>
2020-10-26 17:34:27 -0300

    variedades

--- js/jsons.js
+++ js/jsons.js
@@ -43,7 +43,15 @@ var menu =
 var downloads = 
 [
     {
-        'item': 'O que é Fé',
+        'item': 'O que é Fé - R. C. Sproul',
         'link': 'https://drive.google.com/file/d/1b8Dy-8u-JMbNJhmOfu6AE8Np-aA-P98u/view?usp=sharing'
+    },
+    {
+        'item': 'Como ser Salvo - J. C. Ryle',
+        'link': 'https://drive.google.com/file/d/1T3l-hWlgq6Z2HiU43a8oZL0y9_8oCwQi/view?usp=sharing'
+    },
+    {
+        'item': 'O que significa ser nascido de novo - J. C. Ryle',
+        'link': 'https://drive.google.com/file/d/1BjHjTGfL2J7YnnTUOGbLiFK50XpDocrW/view?usp=sharing'
     }
 ]
