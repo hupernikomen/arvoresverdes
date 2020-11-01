28e40572012ca3aa5dec11a7ca1a2d2ce63eb621
Wilson Ramos <hupcontato@gmail.com>
2020-10-31 21:07:18 -0300

    cores

--- js/evento.js
+++ js/evento.js
@@ -1,70 +1,112 @@
 var semana = ["Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sábado"];
-
-prog = [[],
-[{ 'dia': 1, 'evt': 'Culto - Exemplo', 'hr': '19:20', 'loc': 'Templo Batista em Arvores Verdes' }],
-[{ 'dia': 2, 'evt': 'Culto - Exemplo', 'hr': '19:20', 'loc': 'Templo Batista em Arvores Verdes' }],
-[{ 'dia': 3, 'evt': 'Culto - Exemplo', 'hr': '19:20', 'loc': 'Templo Batista em Arvores Verdes' }],
-[{ 'dia': 4, 'evt': 'Culto - Exemplo', 'hr': '19:20', 'loc': 'Templo Batista em Arvores Verdes' }],
-[{ 'dia': 5, 'evt': 'Culto - Exemplo', 'hr': '19:20', 'loc': 'Templo Batista em Arvores Verdes' }],
-[{ 'dia': 6, 'evt': 'Culto - Exemplo', 'hr': '19:20', 'loc': 'Templo Batista em Arvores Verdes' }],
-[{ 'dia': 7, 'evt': 'Culto - Exemplo', 'hr': '19:20', 'loc': 'Templo Batista em Arvores Verdes' }],
-[{ 'dia': 8, 'evt': 'Culto - Exemplo', 'hr': '19:20', 'loc': 'Templo Batista em Arvores Verdes' }],
-[{ 'dia': 9, 'evt': 'Culto - Exemplo', 'hr': '19:20', 'loc': 'Templo Batista em Arvores Verdes' }],
-[{ 'dia': 10, 'evt': 'Culto - Exemplo', 'hr': '19:20', 'loc': 'Templo Batista em Arvores Verdes' }],
-[{ 'dia': 11, 'evt': 'Culto - Exemplo', 'hr': '19:20', 'loc': 'Templo Batista em Arvores Verdes' }],
-[{ 'dia': 12, 'evt': 'Culto - Exemplo', 'hr': '19:20', 'loc': 'Templo Batista em Arvores Verdes' }],
-[{ 'dia': 13, 'evt': 'Culto - Exemplo', 'hr': '19:20', 'loc': 'Templo Batista em Arvores Verdes' }],
-[{ 'dia': 14, 'evt': 'Culto - Exemplo', 'hr': '19:20', 'loc': 'Templo Batista em Arvores Verdes' }],
-[{ 'dia': 15, 'evt': 'Culto - Exemplo', 'hr': '19:20', 'loc': 'Templo Batista em Arvores Verdes' }],
-[{ 'dia': 16, 'evt': 'Culto - Exemplo', 'hr': '19:20', 'loc': 'Templo Batista em Arvores Verdes' }],
-[{ 'dia': 17, 'evt': 'Culto - Exemplo', 'hr': '19:20', 'loc': 'Templo Batista em Arvores Verdes' }],
-[{ 'dia': 18, 'evt': 'Culto - Exemplo', 'hr': '19:20', 'loc': 'Templo Batista em Arvores Verdes' }],
-[{ 'dia': 19, 'evt': 'Culto - Exemplo', 'hr': '19:20', 'loc': 'Templo Batista em Arvores Verdes' }],
-[{ 'dia': 20, 'evt': 'Culto - Exemplo', 'hr': '19:20', 'loc': 'Templo Batista em Arvores Verdes' }],
-[{ 'dia': 21, 'evt': 'Culto - Exemplo', 'hr': '19:20', 'loc': 'Templo Batista em Arvores Verdes' }],
-[],
-[],
-[{ 'dia': 24, 'evt': 'Culto - Exemplo', 'hr': '12:20', 'loc': 'Templo Batista em Arvores Verdes' }],
-[{ 'dia': 25, 'evt': 'Culto - Exemplo', 'hr': '19:20', 'loc': 'Templo Batista em Arvores Verdes' }],
-[{ 'dia': 26, 'evt': 'Culto - Exemplo', 'hr': '19:20', 'loc': 'Templo Batista em Arvores Verdes' }],
-[],
-[],
-[],
-[{ 'dia': 30, 'evt': 'Culto - Exemplo', 'hr': '19:20', 'loc': 'Templo Batista em Arvores Verdes' }],
-[{ 'dia': 31, 'evt': 'Culto - Exemplo', 'hr': '19:20', 'loc': 'Templo Batista em Arvores Verdes' }]
+var cultoDominical = {
+    'bg':'#5a9dc9',
+    'evt': 'Culto - Louvor e Pregação',
+    'hr': '18:00',
+    'loc': 'Templo Batista Arvores Verdes'
+}
+var ebd = {
+    'bg':'#b0c940',
+    'evt': 'EBD',
+    'hr': '17:00',
+    'loc': 'Templo Batista Arvores Verdes'
+}
+var oracao = {
+    'bg':'#4cc25c',
+    'evt': 'Oração e Doutrina',
+    'hr': '19:30',
+    'loc': 'Templo Batista Arvores Verdes'
+}
+var pizza = {
+    'bg':'#4c58c2',
+    'evt': 'Festival de Pizza',
+    'hr': '18:30',
+    'loc': 'Templo Batista Arvores Verdes'
+}
+var reuniaoH = {
+    'bg':'#4c71c2',
+    'evt': 'Reuniao dos Homens',
+    'hr': '18:30',
+    'loc': 'Casa do Irmão ...'
+}
+var reuniaoM = {
+    'bg':'#8d4cc2',
+    'evt': 'Reuniao das Mulheres',
+    'hr': '18:30',
+    'loc': 'Casa da Irmã ...'
+}
+prog = [[], //00
+[ebd, cultoDominical],
+[], //02
+[], //03
+[], //04
+[], //05
+[], //06
+[oracao], //07
+[ebd, cultoDominical], //08
+[], //09
+[], //10
+[reuniaoH, reuniaoM], //11
+[], //12
+[oracao], //13
+[],//14
+[ebd, cultoDominical], //15
+[], //16
+[], //17
+[], //18
+[], //19
+[oracao], //20
+[pizza], //21
+[ebd, cultoDominical], //22
+[], //23
+[], //24
+[], //25
+[], //26
+[oracao],//27
+[], //28
+[ebd, cultoDominical], //29
+[], //30
+[] //31
 ]
 
 var data = new Date()
 var dMesComp, dMes = data.getDate()
+var mes = data.getMonth()
+var fimStatus = false
+
+function fimMes() {
+    for (var j = dMes; j < prog.length; j++) {
+        prog[j].length == 0 && fimStatus === false ? fimStatus = false : fimStatus = true
+    }
+    fimStatus == false ? mes++ : mes
+    console.log('ainda ha eventos esse mes?', fimStatus)
+    console.log('mes atual', mes)
+}
+fimMes()
 
 while (prog[dMes] == '') {
     dMes++
-    if (prog[dMes] == null) { dMes = 0 }
+    if (prog[dMes] == null) {
+        dMes = 0
+    }
 }
-
-for (var i = 0; i < prog[dMes].length; i++) {
-    var item = prog[dMes][i]
+var index = Object.keys(prog)
+prog[dMes].map((i) => {
     var post = `
-    <div class="item-prog">
-        <span class="tag-dia">${item.dia}</span>
-        <div class="ml-2">
-            <h6 class="tag-evt">${item.evt}</h6>
-            <span class="tag-loc">${item.loc}</span>
-            <span class="tag-diaD">${item.dia == dMesComp ? 'Hoje' : semana[dSem(item.dia)]}</span> - 
-            <span class="tag-hora">${item.hr}</span>
-        </div>
-
+    <div>
+        <h6 class="tag-evt mb-3">
+            <span class="tag-dia mr-3" style="background: ${i.bg}">${index[dMes]}</span>
+            ${i.evt}
+        </h6>
+        <span class="tag-loc">${i.loc}</span>
+        <span class="tag-diaD">${index[dMes] == dMesComp ? 'Hoje' : semana[dSem(index[dMes])]}</span> - 
+        <span class="tag-hora">${i.hr}</span>
     </div>
-    `
-    $('.prog').append(post)
+`
+    $('.item-prog').append(post)
+})
 
-}
-var btnMore = `
-<a href="eventos.html" class="more"><i class="far fa-calendar-plus"></i></a>`
-
-$('.item-prog').append(btnMore)
 
 function dSem(dia) {
-    var d = new Date(data.getFullYear(), data.getMonth(), dia)
-    return d.getDay()
+    return new Date(data.getFullYear(), mes, dia).getDay()
 }
--- js/eventos.js
+++ js/eventos.js
@@ -2,15 +2,13 @@ for (var i = dMes; i < prog.length; i++) {
 
     prog[i].map((item) => {
         var post = `
-    <div class="item-prog my-4">
-        <div class="tag-dia">${item.dia}</div>
-        <div class="ml-2">
-            <h6 class="tag-evt">${item.evt}</h6>
+        
+        <div>
+            <h6 class="tag-evt mb-3"><span class="tag-dia mr-3" style="background: ${item.bg}">${index[i]}</span>${item.evt}</h6>
             <span class="tag-loc">${item.loc}</span>
-            <span class="tag-diaD">${item.dia == dMesComp ? 'Hoje' : semana[dSem(item.dia)]}</span> - 
+            <span class="tag-diaD">${index[i] == dMesComp ? 'Hoje' : semana[dSem(index[i])]}</span> - 
             <span class="tag-hora">${item.hr}</span>
         </div>
-    </div>
     `
         $('.itens-prog').append(post)
     })
