678a822cb0a983f70c0e354b46c001aa9d208fb3
Wilson Ramos <hupcontato@gmail.com>
2020-11-02 16:13:02 -0300

    khb

--- js/eventos.js
+++ js/eventos.js
@@ -1,15 +1,15 @@
-for (var i = dMes; i < prog.length; i++) {
+for (var i = diaDoMes; i < progs.length; i++) {
 
-    prog[i].map((item) => {
+    progs[i].map((item) => {
         var post = `
         
         <div class="mb-4">
             <h6 class="tag-evt my-2">
-                <span class="tag-dia mr-3" style="background: ${item.bg}">${index[i]} / ${mes}</span>
+                <span class="tag-dia mr-3" style="background: ${item.bg}">${Object.keys(progs)[i]} / ${mes}</span>
                 ${item.evt}
             </h6>
             <span class="tag-loc">${item.loc}</span>
-            <span class="tag-diaD">${index[i] == dMesComp ? 'Hoje' : semana[dSem(index[i])]}</span> - 
+            <span class="tag-diaD">${Object.keys(progs)[i] == dMesComp ? 'Hoje' : arrDiasDaSemana[diaDaSemana(Object.keys(progs)[i])]}</span> - 
             <span class="tag-hora">${item.hr}</span>
         </div>
     `
