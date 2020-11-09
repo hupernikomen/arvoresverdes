cd236b17011c90063d692c05ec0a736d7325317a
Wilson Ramos <hupcontato@gmail.com>
2020-11-09 14:22:22 -0300

    k

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
 
-
+app.use((req, res, next) => { //Cria um middleware onde todas as requests passam por ele 
+    if ((req.headers["x-forwarded-proto"] || "").endsWith("http")) //Checa se o protocolo informado nos headers é HTTP 
+        res.redirect(`https://${req.headers.host}${req.url}`); //Redireciona pra HTTPS 
+    else //Se a requisição já é HTTPS 
+        next(); //Não precisa redirecionar, passa para os próximos middlewares que servirão com o conteúdo desejado 
+});
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
