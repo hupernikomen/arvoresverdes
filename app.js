1d4a5a8506b44e2b49568fdfc80bd7d2b1d78b2c
Wilson Ramos <hupcontato@gmail.com>
2020-11-09 10:52:38 -0300

    j

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
 
-app.static('/public', __dirname + '/css')
-// app.use(express.static(__dirname + '/public'));
+
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/html/index.html')
