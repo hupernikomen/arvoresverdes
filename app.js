a7588a425aaae4b9fae0293f7592dbe046ff27b2
Wilson Ramos <hupcontato@gmail.com>
2020-11-10 21:38:59 -0300

    k

--- app.js
+++ app.js
@@ -24,9 +24,8 @@ app.get('/biblioteca', (req, res) => {
 app.get('/contato', (req, res) => {
     res.sendFile(__dirname + '/html/contato.html')
 })
-app.get('/blog' , (req, res) => {
+app.get('/blog', (req, res) => {
     res.sendFile(__dirname + '/html/blog.html')
-    
 })
 app.get('/sitemap', (req, res) => {
     res.sendFile(__dirname + '/sitemap.xml')
--- public/js/blog.js
+++
@@ -1,2 +0,0 @@
-console.log(id)
-alert('oi')
