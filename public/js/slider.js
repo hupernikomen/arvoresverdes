bf355ff48b30b245f98bb4c2cf5d50831cfa2912
Wilson Ramos <hupcontato@gmail.com>
2020-11-18 11:11:43 -0300

    l

--- public/js/slider.js
+++ public/js/slider.js
@@ -7,8 +7,7 @@ var slide = [
     {
         'img': '<img class="d-block w-100" src="imagens/mobile-slide-2.webp">',
         'title': 'Culto Infantil',
-        'notice': 'Jesus disse: "Deixai vir a mim as criancinhas" - Mt 19:14. Traga seu filho ao culto infantil',
-        'button': 'Saiba +',
+        'notice': 'Jesus disse: "Deixai vir a mim as criancinhas" - Mt 19:14. Traga seu filho ao culto infantil'
     }
 ]
 
@@ -16,12 +15,10 @@ for (var i = 0; i < slide.length; i++) {
     var slideItem = `
         <div class="carousel-item ${i == 0 ? 'active' : ''}">
             ${slide[i].img}
-            <div class="conteudo-slide">
+            <a href="/${i}" class="conteudo-slide">
                 <h3 class=" mx-4">${slide[i].title}</h3>
                 <p class=" mx-4">${slide[i].notice}</p>
-                ${slide[i].button != undefined ? `<a class="btn-slide mx-4" href="/info/${i}">${slide[i].button}</a>`: ''}
-                
-            </div>
+            </a>
         </div>
         `
     $('.carousel-inner').append(slideItem)
