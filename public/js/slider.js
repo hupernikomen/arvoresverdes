bcbdb7a3295be135936cca3ba51e5fa7674cbf62
Wilson Ramos <hupcontato@gmail.com>
2020-11-18 11:03:05 -0300

    l

--- public/js/slider.js
+++ public/js/slider.js
@@ -1,15 +1,14 @@
 var slide = [
     {
         'img': '<img class="d-block w-100" src="imagens/mobile-slide-1.webp">',
-        'title': 'EVANGELISMO',
-        'notice': 'Dia 25/01 no Morro do Papagaio',
-        'button': 'PARTICIPE',
+        'title': 'Evangelismo',
+        'notice': 'Dia 25/01 no Morro do Papagaio'
     },
     {
         'img': '<img class="d-block w-100" src="imagens/mobile-slide-2.webp">',
-        'title': 'CULTO INFANTIL',
+        'title': 'Culto Infantil',
         'notice': 'Jesus disse: "Deixai vir a mim as criancinhas" - Mt 19:14. Traga seu filho ao culto infantil',
-        'button': 'SAIBA MAIS',
+        'button': 'Saiba +',
     }
 ]
 
@@ -18,9 +17,10 @@ for (var i = 0; i < slide.length; i++) {
         <div class="carousel-item ${i == 0 ? 'active' : ''}">
             ${slide[i].img}
             <div class="conteudo-slide">
-                <h3 class=" mx-5">${slide[i].title}</h3>
-                <p class=" mx-5">${slide[i].notice}</p>
-                <a class=" mx-5" href="/info/${i}">${slide[i].button}</a>
+                <h3 class=" mx-4">${slide[i].title}</h3>
+                <p class=" mx-4">${slide[i].notice}</p>
+                ${slide[i].button != undefined ? `<a class="btn-slide mx-4" href="/info/${i}">${slide[i].button}</a>`: ''}
+                
             </div>
         </div>
         `
