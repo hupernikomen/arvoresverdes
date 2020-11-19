3e03c530a33285bed0058f3d05a3de1ee895ada0
Wilson Ramos <hupcontato@gmail.com>
2020-11-19 06:47:17 -0300

    l

--- public/js/slider.js
+++ public/js/slider.js
@@ -1,20 +1,23 @@
 var slide = [
     {
-        'img': '<img class="d-block w-100" src="imagens/mobile-slide-1.webp">',
+        'imgmob': '<img class="d-block w-100" src="imagens/banners/mobile-slide-1.webp">',
+        'imgdesk': '<img class="d-block w-100" src="imagens/banners/evangelismo.jpg">',
         'title': 'Evangelismo',
         'notice': 'Dia 25/01 no Morro do Papagaio'
     },
     {
-        'img': '<img class="d-block w-100" src="imagens/mobile-slide-2.webp">',
+        'imgmob': '<img class="d-block w-100" src="imagens/banners/mobile-slide-2.webp">',
+        'imgdesk': '<img class="d-block w-100" src="imagens/banners/oracao.jpg">',
         'title': 'Culto Infantil',
         'notice': 'Jesus disse: "Deixai vir a mim as criancinhas" - Mt 19:14. Traga seu filho ao culto infantil'
     }
 ]
 
 for (var i = 0; i < slide.length; i++) {
+    var larg = window.screen.width
     var slideItem = `
         <div class="carousel-item ${i == 0 ? 'active' : ''}">
-            ${slide[i].img}
+            ${larg > 1024 ? slide[i].imgdesk : slide[i].imgmob}
             <a href="/${i}" class="conteudo-slide">
                 <h3 class=" mx-4">${slide[i].title}</h3>
                 <p class=" mx-4">${slide[i].notice}</p>
@@ -23,4 +26,3 @@ for (var i = 0; i < slide.length; i++) {
         `
     $('.carousel-inner').append(slideItem)
 }
-
