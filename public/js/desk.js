070cc7c14746c88489d718533ac32e303ec20d08
Wilson Ramos <hupcontato@gmail.com>
2020-12-23 15:41:10 -0300

    l

--- public/js/desk.js
+++ public/js/desk.js
@@ -1,6 +1,6 @@
 let larg = window.screen.width
 if (larg > 768) {
   window.location = 'https://www.ibavthe.com/desk'
-} else {
+} else if (larg <= 768){
   window.location = 'https://www.ibavthe.com'
 }
